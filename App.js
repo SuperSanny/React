c23201d6f962552490eef10d96393b91f1446c3f
Sanny Kumar <ksanny556@gmail.com>
2023-06-08 00:03:29 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement(
+    "H1", {
+        id: "heading"
+    },
+    "Hello World"
+);
+
+const heading2 = React.createElement(
+    "H2", {
+        id: "heading2"
+    },
+    "Hello World"
+);
